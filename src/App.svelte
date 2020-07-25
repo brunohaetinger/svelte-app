1f723f7a9eb67d63eb46485def50e09c7812e7e8
Bruno Haetinger <bruno.haetinger@thomsonreuters.com>
2020-07-24 22:04:49 -0300

    Svelte tutorial

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,30 @@
 <script>
+	import Stepper from './Stepper.svelte';
 	export let name;
+	let stepperValue = 0;
+	$: isEven = stepperValue % 2 == 0;
+
+	function handleStepperEvents(event){
+		console.log(event);
+		const value = event.detail.value;
+		if(value === '-'){ 
+			stepperValue--;
+		}else {
+			stepperValue++;
+		}
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Stepper on:btnValue={handleStepperEvents} value={stepperValue}></Stepper>
+
+	{#if isEven}
+		<h1>{stepperValue} is EVEN!</h1>
+	{:else}
+		<h1>{stepperValue} is ODD!</h1>
+	{/if}
 </main>
 
 <style>
---
+++ src/Stepper.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let value;
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+    const dispatchValue = (btnValue)=>{
+        dispatch('btnValue', {
+            value: btnValue
+        });
+    }
+</script>
+
+<main>
+	<button on:click={() => dispatchValue('-')}>-</button>
+    <h1>{value}</h1>
+	<button on:click={() => dispatchValue('+')}>+</button>
+</main>
+
+<style>
+	main{
+        display: flex;
+        justify-content: center;
+        align-items: center
+    }
+
+    button{
+        border: 1px gray solid;
+        height: 40px;
+        width: 40px;
+        border-radius: 25%;
+        font-weight: bolder;
+        margin: 5px;
+    }
+</style>
